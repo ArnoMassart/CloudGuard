95c15a621a4965b779429c37be8bd1839f683bd1
Arno Massart <arno.massart@outlook.com>
2026-02-24 10:47:01 +0100

    Changed the user repository identifier to match the user model.

--- api/src/main/java/com/cloudmen/cloudguard/repository/UserRepository.java
+++ api/src/main/java/com/cloudmen/cloudguard/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.math.BigInteger;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, BigInteger> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
