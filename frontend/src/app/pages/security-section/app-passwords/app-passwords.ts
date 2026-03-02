4129247645f11efe20a203da439f90ab2ff92a40
anna <03ayv21@gmail.com>
2026-03-02 11:49:30 +0100

    Add simple frontend

--- frontend/src/app/pages/security-section/app-passwords/app-passwords.ts
+++ frontend/src/app/pages/security-section/app-passwords/app-passwords.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AppPasswordsService } from '../../../services/app-password-service';
+
+export interface AppPassword {
+  userEmail: string;
+  codeId: number;
+  name: string;
+  createdAt: Date;
+  lastUsedAt: Date | null;
+}
 
 @Component({
   selector: 'app-app-passwords',
@@ -6,6 +15,18 @@ import { Component } from '@angular/core';
   templateUrl: './app-passwords.html',
   styleUrl: './app-passwords.css',
 })
-export class AppPasswords {
+export class AppPasswords implements OnInit{
+  readonly #appPasswordsService = inject(AppPasswordsService);
+  readonly appPasswords = signal<AppPassword[]>([]);
 
+  ngOnInit(): void{
+    this.#loadAppPasswords();
+  }
+
+  #loadAppPasswords() {
+    this.#appPasswordsService.getAppPasswords().subscribe({
+      next: (passwords) => this.appPasswords.set(passwords),
+      error: (err) => console.error('Failed to load app passwords', err),
+    });
+  }
 }
---
+++ frontend/src/app/services/app-password-service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { RouteService } from './route-service';
+import { HttpClient } from '@angular/common/http';
+
+export interface AppPassword {
+  userEmail: string;
+  codeId: number;
+  name: string;
+  createdAt: Date;
+  lastUsedAt: Date | null;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppPasswordsService {
+    readonly #API_URL = RouteService.getBackendUrl('/google/app-passwords');
+    readonly #http = inject(HttpClient);
+
+    public getAppPasswords() {
+        return this.#http.get<AppPassword[]>(`${this.#API_URL}`, {
+            withCredentials: true
+        });
+    }
+}
