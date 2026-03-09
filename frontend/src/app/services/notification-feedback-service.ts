30b8c3a214ebbc4764d8677ce692f723c119b076
anna <03ayv21@gmail.com>
2026-03-09 15:14:54 +0100

    Add frontend

---
+++ frontend/src/app/services/notification-feedback-service.ts
@@ -0,0 +1,31 @@
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { RouteService } from './route-service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationFeedbackService {
+    readonly #API_URL = RouteService.getBackendUrl('/notifications/feedback');
+    readonly #http = inject(HttpClient);
+
+    submitFeedback(source: string, notificationType: string, feedbackText: string): Observable<void>{
+        return this.#http.post<void>(this.#API_URL,{
+            source,
+            notificationType,
+            feedbackText
+        },{
+            withCredentials: true,
+        });
+    }
+
+    hasFeedback(source: string, notificationType: string): Observable<boolean> {
+        return this.#http.get<boolean>(this.#API_URL, {
+            params: { source, notificationType },
+            withCredentials: true,
+        });
+    }
+
+}
