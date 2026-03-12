c084ba627f2b72c0d81d7cf7a8db2d460e40f287
Arno Massart <arno.massart@outlook.com>
2026-03-12 11:16:36 +0100

    Changed the style of the logout dialog.

--- frontend/src/app/components/log-out-dialog/log-out-dialog.ts
+++ frontend/src/app/components/log-out-dialog/log-out-dialog.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { AppIcons } from '../../shared/AppIcons';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-log-out-dialog',
-  imports: [MatButtonModule, MatCheckboxModule, MatDialogModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatDialogModule, LucideAngularModule],
   templateUrl: './log-out-dialog.html',
   styleUrl: './log-out-dialog.css',
 })
-export class LogOutDialog {}
+export class LogOutDialog {
+  readonly Icons = AppIcons;
+}
