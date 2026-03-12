2cd77c07fdf7f05167e5418ed8b40cc08c886f8a
anna <03ayv21@gmail.com>
2026-03-12 14:05:00 +0100

    Change profile popup design

--- frontend/src/app/pages/profile/profile.ts
+++ frontend/src/app/pages/profile/profile.ts
@@ -1,15 +1,18 @@
 import { Component, inject, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
 import { CustomAuthService } from '../../auth/custom-auth-service';
 import { UserService } from '../../services/user-service';
+import { AppIcons } from '../../shared/AppIcons';
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
+  readonly Icons = AppIcons;
   readonly authService = inject(CustomAuthService);
   readonly currentUser = this.authService.currentUser;
   readonly userService = inject(UserService);
