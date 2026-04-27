4d6cd267587da5b9621e7c7269fefb7cb9e3cf6a
Arno Massart <arno.massart@outlook.com>
2026-04-27 16:07:14 +0200

    Added javadoc for half of the DTO/organization.

--- api/src/main/java/com/cloudmen/cloudguard/dto/organization/DatabaseOrgResponse.java
+++ api/src/main/java/com/cloudmen/cloudguard/dto/organization/DatabaseOrgResponse.java
@@ -3,6 +3,15 @@ package com.cloudmen.cloudguard.dto.organization;
 import com.cloudmen.cloudguard.domain.model.Organization;
 import java.util.List;
 
+/**
+ * A Data Transfer Object (DTO) representing a paginated response for a list of organizations. <p>
+ *
+ * This record encapsulates a subset of organizations retrieved from the database for the current page, along with a
+ * token to fetch subsequent pages.
+ *
+ * @param organizations the list of {@link Organization} entities retrieved for the current page of results
+ * @param nextPageToken the token used to request the next page of results, or {@code null} if there are no more pages
+ */
 public record DatabaseOrgResponse(
         List<Organization> organizations,
         String nextPageToken
