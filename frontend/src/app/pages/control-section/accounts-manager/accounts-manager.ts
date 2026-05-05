4b765720e8a3e16cea8a6a4d71f78aaf146b1345
Anna Yang <anna.yang@student.ap.be>
2026-05-05 14:29:21 +0200

    Change placement of account management buttons
    # Conflicts:
    #	frontend/src/app/pages/control-section/accounts-manager/accounts-manager.ts

--- frontend/src/app/pages/control-section/accounts-manager/accounts-manager.ts
+++ frontend/src/app/pages/control-section/accounts-manager/accounts-manager.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { PageHeader } from '../../../components/page-header/page-header';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { AppIcons } from '../../../shared/AppIcons';
@@ -8,6 +8,8 @@ import { AccountSectionType } from '../../../models/account/AccountSectionType';
 import { CommonModule } from '@angular/common';
 import { AccountsManagerUsers } from './accounts-manager-users/accounts-manager-users';
 import { AccountsManagerOrganizations } from './accounts-manager-organizations/accounts-manager-organizations';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/user-service';
 import { PageContentWrapper } from '../../../components/page-content-wrapper/page-content-wrapper';
 
 @Component({
@@ -28,6 +30,9 @@ import { PageContentWrapper } from '../../../components/page-content-wrapper/pag
 export class AccountsManager implements OnInit {
   readonly Icons = AppIcons;
 
+  readonly #router = inject(Router);
+  readonly userService = inject(UserService);
+
   readonly currentSection = signal<AccountSectionType>('USERS');
 
   // ==========================================
@@ -35,6 +40,8 @@ export class AccountsManager implements OnInit {
   // ==========================================
   ngOnInit(): void {
     const section = sessionStorage.getItem('account-section');
+    this.loadAccessRequestsCount();
+    this.loadDeniedCount();
 
     if (section === 'USERS' || section === 'ORGANIZATIONS') {
       this.currentSection.set(section);
@@ -61,5 +68,31 @@ export class AccountsManager implements OnInit {
       'border-transparent text-slate-500 hover:text-slate-700 cursor-pointer': !isActive,
       'flex items-center gap-2 pb-3 border-b-2 -mb-[1px] transition-all': true,
     };
+  }
+
+  loadAccessRequestsCount() {
+    this.userService.refreshRequestedCount().subscribe({
+      next: (res) => {},
+      error: (err) => {
+        console.error('Error getting the access request count', err);
+      },
+    });
+  }
+
+  loadDeniedCount() {
+    this.userService.refreshDeniedCount().subscribe({
+      next: (res) => {},
+      error: (err) => {
+        console.error('Error getting the access request count', err);
+      },
+    });
+  }
+
+  goToRequests() {
+    this.#router.navigate(['accounts-manager/requests']);
+  }
+
+  goToDenied() {
+    this.#router.navigate(['/accounts-manager/denied-list']);
   }
 }
